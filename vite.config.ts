373ace7b0feafaedf782d21dc27cc79fbfd1e05a
Karim Elsayad <karimelsayad1999@gmail.com>
2025-05-22 00:03:48 +0300

    Replace Tailwind postcss plugin with vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
 import StimulusHMRPlugin from "vite-plugin-stimulus-hmr";
 import { DisableHtmlReload } from "./vite-plugins/html_hmr.js";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     RubyPlugin(),
     StimulusHMRPlugin(),
     DisableHtmlReload(),
